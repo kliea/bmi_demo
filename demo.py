c81d5bfac2d598d0da2b708e03f156218313f251
kliea <90135283+kliea@users.noreply.github.com>
2021-11-06 00:35:42 +0800

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,21 @@
+# bmi-demo-project
+height = int(input("please input your height in cm: "))
+weight = int(input("please input your weight in kg: "))
+h = 100
+print("\n")
+#bmi_calculator
+initialheight = height / h
+finalheight = round(initialheight ** 2, 2)
+bmi = round(weight / finalheight, 1)
+print("You're BMI is: " + str(bmi) + " and")
+
+#bmi_categories
+if bmi <= 18.5:
+    print("You're Underweight")
+elif bmi > 18.5 and bmi <= 24.9:
+    print("You have normal weight")
+elif bmi > 25 and bmi <= 29.9:
+    print("You're Overweight")
+else:
+    print("You have Obesity")
+    
